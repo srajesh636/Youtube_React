05a5cfc944a3f040c39dcba308cd9629d11a62b2
srajesh636 <srajesh636@gmail.com>
2018-06-24 15:48:50 +0530

    added font awesome

--- src/components/listitem.js
+++ src/components/listitem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../css/item.css";
+import 'font-awesome/css/font-awesome.min.css';
 class ListItem extends React.Component {
   render() {
 
@@ -16,7 +17,11 @@ class ListItem extends React.Component {
     <img src={image} className='card-image' />
     <div className="card-title">
       {snippet.title}
+
+
     </div>
+    <div className="youtube-logo">  <i className="fa fa-youtube" aria-hidden="true"></i></div>
+
     </div>
 
     );
