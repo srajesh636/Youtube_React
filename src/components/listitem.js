f20722642a84e1d1a4b7ccecd1693c32a9d28338
srajesh636 <srajesh636@gmail.com>
2018-06-24 15:17:46 +0530

    cmmiting

--- src/components/listitem.js
+++ src/components/listitem.js
@@ -1,35 +1,23 @@
 import React from "react";
 import "../css/item.css";
+import 'font-awesome/css/font-awesome.min.css';
 class ListItem extends React.Component {
   render() {
-
-    let data = this.props.data.id.snippet || undefined;
-    let snippet=this.props.data.snippet;
-
-    let image=snippet.thumbnails.medium.url;
-
-
+    let data = this.props.data.id.snippet || { main: "" };
 
     let url = "https://www.youtube.com/embed/" + this.props.data.id.videoId;
 
-    return (<div className="card" onClick={this.props.getindex} >
-    <img src={image} className='card-image' />
-    <div className="card-title">
-      {snippet.title}
-    </div>
-    </div>
-
+    return (
+      <div className="item">
+        <iframe width="300" height="250" src={url} />
+        <div className="description">
+          <button onClick={this.props.getindex} class="button">
+            play
+          </button>
+        </div>
+      </div>
     );
   }
 }
 
 export default ListItem;
-
-// <div className="item">
-//   <iframe width="300" height="250" src={url} />
-//   <div className="description">
-//     <button onClick={this.props.getindex} class="button">
-//       play
-//     </button>
-//   </div>
-// </div>
